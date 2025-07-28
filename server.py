2c0a4d8f7102a0daab9f71976200af2384ab7af9
creaprint3d <167899369+creaprint3d@users.noreply.github.com>
2025-07-28 02:18:51 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-last_description = ""  # Variable globale pour stocker la dernière description reçue
+last_description = ""  # Variable globale
 
 @app.route('/receive_gpt_description', methods=['POST'])
 def receive_description():
@@ -17,4 +18,5 @@ def get_last_description():
     return jsonify({"description": last_description}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+    port = int(os.environ.get("PORT", 5000))  # Récupère le port utilisé par Render
+    app.run(host='0.0.0.0', port=port)
